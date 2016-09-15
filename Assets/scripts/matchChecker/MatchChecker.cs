29fb7f2fe9f11270c45f3f886159f023215a1677
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-09-15 21:56:15 +0100

    As above but for right hand side checks, as well as checking to see if there is a matching item in the same row, but separated from the two matching by a non matching item

--- Assets/scripts/matchChecker/MatchChecker.cs
+++ Assets/scripts/matchChecker/MatchChecker.cs
@@ -70,4 +70,82 @@ public class MatchChecker {
 		return null;
 
 	}
+
+	public static List<GameObject> CheckHorizontal1 (int row, int column, CandyArray candies) {
+
+		if (column <= GameVariables.columns - 3) {
+
+			if (candies [row, column].GetComponent<Candy> ().IsSameType (candies [row, column + 1].GetComponent<Candy>())) {
+
+				if (row >= 1 && column <= GameVariables.columns - 3) {
+
+					if (candies [row, column].GetComponent<Candy> ().IsSameType (candies [row - 1, column + 2].GetComponent<Candy> ())) {
+						return new List<GameObject> {
+							candies [row, column],
+							candies [row, column + 1],
+							candies [row - 1, column + 2]
+						};
+					}
+
+					if (row <= GameVariables.rows - 2 && column <= GameVariables.columns - 3) {
+
+						if (candies [row, column].GetComponent<Candy> ().IsSameType (candies [row + 1, column + 2].GetComponent<Candy> ())) {
+							return new List<GameObject> {
+								candies [row, column],
+								candies [row, column + 1],
+								candies [row + 1, column + 2]
+							};
+						}
+
+					}
+
+				}
+
+			}
+
+		}
+
+		return null;
+
+	}
+
+	public static List<GameObject> CheckHorizontal2 (int row, int column, CandyArray candies) {
+
+		if (column <= GameVariables.columns - 4) {
+
+			if (
+				candies [row, column].GetComponent<Candy> ().IsSameType (candies [row, column + 1].GetComponent<Candy>()) &&
+				candies [row, column].GetComponent<Candy> ().IsSameType (candies [row, column + 3].GetComponent<Candy>())
+			) {
+				
+				return new List<GameObject> {
+					candies [row, column],
+					candies [row, column + 1],
+					candies [row, column + 3]
+				};
+
+			}
+
+		}
+
+		if (column >= 2 && column <= GameVariables.columns - 2) {
+
+			if (
+				candies [row, column].GetComponent<Candy> ().IsSameType (candies [row, column + 1].GetComponent<Candy>()) &&
+				candies [row, column].GetComponent<Candy> ().IsSameType (candies [row, column - 2].GetComponent<Candy>())
+			) {
+
+				return new List<GameObject> {
+					candies [row, column],
+					candies [row, column + 1],
+					candies [row, column - 2]
+				};
+
+			}
+
+		}
+
+		return null;
+
+	}
 }
