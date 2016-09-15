100e92f07cc7f8703a52cff787d347484832882c
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-09-15 18:38:55 +0100

    Create GameVariables

---
+++ Assets/scripts/gameVariables/GameVariables.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public static class GameVariables {
+
+	public static int Rows = 12;
+	public static int Columns = 8;
+	public static int MinumumMatches = 3;
+	public static int MinumumMatchesForBonus = 4;
+	public static int Match3Score = 100;
+	public static int SubsequelMatchScore = 1000;
+
+	public static float AnimationDuration = 0.2f;
+	public static float MoveAnimationDuration = 0.05f;
+	public static float ExplosionAnimationDuration = 0.3f;
+	public static float WaitBeforePotentialMatchesCheck = 2f;
+	public static float OpacityAnimationDelay = 0.05f;
+
+}
