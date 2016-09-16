123766893d8953d1ad39a5d046af92bb4febc486
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-09-16 18:31:55 +0100

    Add menu and navigation

---
+++ Assets/scripts/menu/MenuManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class MenuManager : MonoBehaviour {
+
+	public void LoadLevel () {
+		SceneManager.LoadScene ("main", LoadSceneMode.Single);
+	}
+
+	public void GoToMenu () {
+		SceneManager.LoadScene ("menu", LoadSceneMode.Single);
+	}
+}
